81f47a67ff2ba1c3459002e0564344a7518ba46a
JakubKallay <jakubko.kallay@gmail.com>
2023-12-02 19:04:23 +0100

    Priklady

---
+++ Priklady.py
@@ -0,0 +1,13 @@
+def Vypisanie():
+    print("""Vyber si príklady na:
+    1.Sčítanie
+    2.Očítanie
+    3.Násobenie
+    4.Delenie
+    """)
+def Vyber_funkcie():
+    x = input("Ktorú funckiu si vyberáš?: ")
+
+Vypisanie()
+
+Vyber_funkcie()
