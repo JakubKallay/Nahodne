8f9b87213db5a867bc07772489907827f4e5e7c5
JakubKallay <jakubko.kallay@gmail.com>
2023-12-02 20:08:19 +0100

    +

--- Priklady.py
+++ Priklady.py
@@ -1,3 +1,5 @@
+import time
+z = 3
 def Vypisanie():
     print("""Vyber si príklady na:
     1.Sčítanie
@@ -6,8 +8,32 @@ def Vypisanie():
     4.Delenie
     """)
 def Vyber_funkcie():
-    x = input("Ktorú funckiu si vyberáš?: ")
+    x = input("Ktorú funckiu si vyberáš?(napíš číslo): ")
+def prvý_príklad():
+    while True:
+        print(a,"+",b)
+        y = float(input("Aký je výsledok?: "))
+        if y == a+b:
+            y = print("Správne, chceš ďalší príklad?(áno/nie): ")
+            if z == "áno":
+                continue
+            else:
+                Vypnutie()
+        else:
+            Zivoty()
+def Vypnutie():
+    print("Kalkulačka sa vypne za 3")
+    time.sleep(0.7)
+    print("                       2")
+    time.sleep(0.7)
+    print("                       1")
+    time.sleep(0.7)
+    exit()
 
-Vypisanie()
-
-Vyber_funkcie()
+def Zivoty():
+    z = z - 1
+    if z > 0:
+        print("Stratil si 1. život")
+    else:
+        print("Ztratil si posledný život")
+        Vypnutie()
