f5e1d715eae7069f0e4ac1983e30d628326e5761
JakubKallay <jakubko.kallay@gmail.com>
2023-12-04 07:31:02 +0100

    +

--- Priklady.py
+++ Priklady.py
@@ -1,5 +1,15 @@
 import time
+
+a = 2
+b = 4
 z = 3
+def Zivoty():
+    r = z - 2
+    if r == 1:
+        print("Stratil si 1. život")
+    else:
+        print("Ztratil si posledný život")
+        Vypnutie()
 def Vypisanie():
     print("""Vyber si príklady na:
     1.Sčítanie
@@ -8,14 +18,15 @@ def Vypisanie():
     4.Delenie
     """)
 def Vyber_funkcie():
-    x = input("Ktorú funckiu si vyberáš?(napíš číslo): ")
-def prvý_príklad():
+    x = int(input("Ktorú funckiu si vyberáš?(napíš číslo): "))
+def Prvý_príklad():
+    z = 3
     while True:
         print(a,"+",b)
         y = float(input("Aký je výsledok?: "))
         if y == a+b:
-            y = print("Správne, chceš ďalší príklad?(áno/nie): ")
-            if z == "áno":
+            pokracovanie = input("Správne, chceš ďalší príklad?(áno/nie): ")
+            if pokracovanie == "áno":
                 continue
             else:
                 Vypnutie()
@@ -30,10 +41,11 @@ def Vypnutie():
     time.sleep(0.7)
     exit()
 
-def Zivoty():
-    z = z - 1
-    if z > 0:
-        print("Stratil si 1. život")
-    else:
-        print("Ztratil si posledný život")
-        Vypnutie()
+
+
+Vypisanie()
+Vyber_funkcie()
+Prvý_príklad()
+
+
+#Ivanova mama je tlsta
