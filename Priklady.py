ff15e93c816ca7a47a337c0a7fd0b63cffd41f3f
JakubKallay <jakubko.kallay@gmail.com>
2023-12-12 10:08:04 +0100

    +

--- Priklady.py
+++ Priklady.py
@@ -1,29 +1,28 @@
+import random
 import time
 z = 3
-a = 2
-b = 4
-
-def Zivoty():
-    print(z)
-    z= z-1
-    #Print z ide ale z = z-1 nie??
-    print("Stratil si 1 živoz")
-    if z == 0:
-        print("Stratil si posledný život")
-        exit()
-
 def Vypisanie():
     print("""Vyber si príklady na:
     1.Sčítanie
-    2.Očítanie
+    2.Odčítanie
     3.Násobenie
     4.Delenie
     """)
 
-
+def Zivoty():
+    global z
+    z= z-1
+    if z >= 1:
+        time.sleep(0.7)
+        print("Nesprávne, stratil si 1 život")
+    else:
+        time.sleep(0.7)
+        print("Nesprávne, strácaš posledný život")
+        Vypnutie()
 def Prvý_príklad():
-
     while True:
+        a = random.randint(1,1000)
+        b = random.randint(1,1000)
         print(a,"+",b)
         y = float(input("Aký je výsledok?: "))
         if y == a+b:
@@ -34,6 +33,53 @@ def Prvý_príklad():
                 Vypnutie()
         else:
             Zivoty()
+
+def Druhý_príklad():
+    while True:
+        a = random.randint(1,1000)
+        b = random.randint(1,1000)
+        print(a,"-",b)
+        y = float(input("Aký je výsledok?: "))
+        if y == a-b:
+            pokracovanie = input("Správne, chceš ďalší príklad?(áno/nie): ")
+            if pokracovanie == "áno":
+                continue
+            else:
+                Vypnutie()
+        else:
+            Zivoty()
+
+def Tretí_príklad():
+
+    while True:
+        a = random.randint(1,30)
+        b = random.randint(1,30)
+        print(a,"*",b)
+        y = float(input("Aký je výsledok?: "))
+        if y == a*b:
+            pokracovanie = input("Správne, chceš ďalší príklad?(áno/nie): ")
+            if pokracovanie == "áno":
+                continue
+            else:
+                Vypnutie()
+        else:
+            Zivoty()
+
+def Štvrtý_príklad():
+    while True:
+        a = random.randint(1,1000)
+        b = random.randint(1,30)
+        print(a,"/",b)
+        y = float(input("Aký je výsledok?: "))
+        if y == a/b:
+            pokracovanie = input("Správne, chceš ďalší príklad?(áno/nie): ")
+            if pokracovanie == "áno":
+                continue
+            else:
+                Vypnutie()
+        else:
+            Zivoty()
+
 def Vypnutie():
     print("Kalkulačka sa vypne za 3")
     time.sleep(0.7)
@@ -44,13 +90,14 @@ def Vypnutie():
     exit()
 
 
-
-Vypisanie()
 chleba = int(input("Ktorú funckiu si vyberáš?(napíš číslo): "))
 if chleba == 1:
     Prvý_príklad()
-else:
-    exit()
+if chleba == 2:
+    Druhý_príklad()
+if chleba == 3:
+    Tretí_príklad()
+if chleba == 4:
+    Štvrtý_príklad()
 
 
-#Ivanova mama je tlsta velmi moc
